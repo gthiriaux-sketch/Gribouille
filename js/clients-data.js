7dbfa2a0bcebad535f23d995daebd59c4273065c
gthiriaux-sketch <gthiriaux@gmail.com>
2026-08-20 09:05:48 +0200

    Update clients-data.js

--- js/clients-data.js
+++ js/clients-data.js
@@ -7,7 +7,7 @@
 const CLIENTS_ENABLED = true;
 
 const CLIENTS_DEFAULTS = [
-  { name: "Couleur Café", url: "https://www.couleurcafe.be/fr/", active: true },
-  { name: "Festival Des Libertés", url: "https://www.festivaldeslibertes.be/2026/fase6?event=26000&_!!_Bienvenue_!!____#26000", active: true },
-  { name: "Esperanzah!", url: "https://www.esperanzah.be", active: true },
+  { name: "Couleur Café", url: "https://www.couleurcafe.be/fr/", logo: "img/clients/couleur-cafe.svg", active: true },
+  { name: "Festival Des Libertés", url: "https://www.festivaldeslibertes.be/2026", logo: "img/clients/festival-des-libertes.svg", active: true },
+  { name: "Esperanzah!", url: "https://www.esperanzah.be", logo: "img/clients/esperanzah.svg", active: true },
 ];
