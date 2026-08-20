85d40115dc190b9eb43742616b35c1c0327b086f
gthiriaux-sketch <gthiriaux@gmail.com>
2026-08-20 09:30:26 +0200

    Change logo paths for clients in clients-data.js
    Updated logo file paths for clients.

--- js/clients-data.js
+++ js/clients-data.js
@@ -7,7 +7,7 @@
 const CLIENTS_ENABLED = true;
 
 const CLIENTS_DEFAULTS = [
-  { name: "Couleur Café", url: "https://www.couleurcafe.be/fr/", logo: "img/clients/couleur-cafe.svg", active: true },
-  { name: "Festival Des Libertés", url: "https://www.festivaldeslibertes.be/2026", logo: "img/clients/festival-des-libertes.svg", active: true },
-  { name: "Esperanzah!", url: "https://www.esperanzah.be", logo: "img/clients/esperanzah.svg", active: true },
+  { name: "Couleur Café", url: "https://www.couleurcafe.be/fr/", logo: "img/clients/cc.svg", active: true },
+  { name: "Festival Des Libertés", url: "https://www.festivaldeslibertes.be/2026", logo: "img/clients/fdl.svg", active: true },
+  { name: "Esperanzah!", url: "https://www.esperanzah.be", logo: "img/clients/espe.svg", active: true },
 ];
